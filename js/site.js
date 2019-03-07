977646fe8b5a9f866f1a6d2fc6df887270f19f63
Kudinov <kudinov@upt24.ru>
2019-03-07 14:41:26 +0300

    html + css

---
+++ js/site.js
@@ -0,0 +1,33 @@
+$(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 3,
+        nav: true,
+        loop: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            480: {
+                items: 2,
+                nav: true
+            },
+            768: {
+                items: 3,
+                nav: true
+            },
+            992: {
+                items: 3,
+                nav: true
+            },
+            1020: {
+                items: 3,
+                nav: true
+            }
+        }
+    });
+});
