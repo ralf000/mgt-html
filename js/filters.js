63f1a5ebe0648dbabd08b0dbc75eaaf5dbe68e2a
Sergey Tikhov <tihov21@mail.ru>
2019-03-13 18:36:43 +0300

    оформление select

--- js/filters.js
+++ js/filters.js
@@ -1,21 +1,23 @@
-$(function () {   
-    
+$(function () {
+
     var arrowDown = '<i class="fa fa-angle-down" aria-hidden="true"></i>';
-    var arrowUp = '<i class="fa fa-angle-up" aria-hidden="true"></i>';    
-    
+    var arrowUp = '<i class="fa fa-angle-up" aria-hidden="true"></i>';
+
     $(".filter-selector").select2({
         minimumResultsForSearch: Infinity, // отключает строку поиска
     });
-    
-    $('.select2-selection__arrow b').hide();     // спрятать дефолтную стрелку
+
+    /*$('.select2-selection__arrow b').hide();*/ // спрятать дефолтную стрелку
     $('.select2-selection__arrow').html(arrowDown);
 
-    $('.filter-selector').on('select2:opening', function (e) {
+    /*$('.filter-selector').on('select2:opening', function (e) {
         $(this).parent().find('.select2-selection__arrow').html(arrowUp);
     });
     
     $('.filter-selector').on('select2:closing', function (e) {
         $(this).parent().find('.select2-selection__arrow').html(arrowDown);
-    });
+    });*/
 
+
+    
 });
