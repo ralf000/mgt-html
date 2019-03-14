e52339a0fffd6b85da778dbf1e232a11691777eb
Sergey Tikhov <tihov21@mail.ru>
2019-03-14 18:49:45 +0300

    news.html, news-item.html, news-item-2.html

--- js/filters.js
+++ js/filters.js
@@ -2,22 +2,15 @@ $(function () {
 
     var arrowDown = '<i class="fa fa-angle-down" aria-hidden="true"></i>';
     var arrowUp = '<i class="fa fa-angle-up" aria-hidden="true"></i>';
+    var calendarIcon = '<img src="holder.js/14x14" class="img-fluid">'; // иконка календаря
 
     $(".filter-selector").select2({
         minimumResultsForSearch: Infinity, // отключает строку поиска
     });
 
-    /*$('.select2-selection__arrow b').hide();*/ // спрятать дефолтную стрелку
-    $('.select2-selection__arrow').html(arrowDown);
-
-    /*$('.filter-selector').on('select2:opening', function (e) {
-        $(this).parent().find('.select2-selection__arrow').html(arrowUp);
-    });
+    $(".filter-selector.date-choose").next().addClass("date-choose"); // добавляем селектору класс date-choose    
     
-    $('.filter-selector').on('select2:closing', function (e) {
-        $(this).parent().find('.select2-selection__arrow').html(arrowDown);
-    });*/
-
-
+    $('.select2-selection__arrow').html(arrowDown); // меняем стандартную стрелку на arrowDown
+    $('.date-choose .select2-selection__arrow').html(calendarIcon); // меняем стандартную стрелку на calendarIcon
     
 });
