a4a4f37c7dc469d4bd066d2edfd6777593f9f965
Sergey Tikhov <tihov21@mail.ru>
2019-03-15 18:45:02 +0300

    search-panel.js, 404.html, event-item.html, etc...

--- js/search-panel.js
+++ js/search-panel.js
@@ -1,18 +1,48 @@
 $(function () {
     
-    var hidden = true;
-    
-    $("header .top-search-link a").on("click", function(){
-        if(hidden) {
-            hidden = false;
-            $(this).find("img").attr("src","../images/close-icon.png");
+    var isSearchHidden = true;
+    var isMenuHidden = true;
+
+    function slideToggleSearch(){
+        if(isSearchHidden) {
+            isSearchHidden = false;
+            $("header .top-search-link a").find("img").attr("src","../images/search-icon-close.png");
             $("#search-panel").slideDown();
         }
         else {
-            hidden = true;
-            $(this).find("img").attr("src","../images/search-icon.png");
+            isSearchHidden = true;
+            $("header .top-search-link a").find("img").attr("src","../images/search-icon.png");
             $("#search-panel").slideUp();
         }
-                                
+    };
+    
+    function slideToggleMenu(){
+        if(isMenuHidden) {
+            isMenuHidden = false;
+            $("header .top-menu-link a").find("img").attr("src","../images/burger-menu-close.png");
+            $("#menu-panel").slideDown();
+        }
+        else {
+            isMenuHidden = true;
+            $("header .top-menu-link a").find("img").attr("src","../images/burger-menu.png");
+            $("#menu-panel").slideUp();
+        }
+    };
+    
+    $("header .top-search-link a").on("click", function(e){
+        e.preventDefault();
+        if(!isMenuHidden){
+            slideToggleMenu();
+            setTimeout(slideToggleSearch, 300);
+        } else slideToggleSearch();
     });
+    
+    $("header .top-menu-link a").on("click", function(e){
+        e.preventDefault();
+        if(!isSearchHidden) {
+            slideToggleSearch();
+            setTimeout(slideToggleMenu, 300);
+        } else slideToggleMenu();
+    });
+    
 });
